a4d40dc0032131ce1eadc0d3d983bcc73a26178f
Michael Oswald <michael.oswald@siemens.com>
2016-08-01 14:42:01 +0200

    Added data imports

--- src/Data/Import.hs
+++ src/Data/Import.hs
@@ -7,9 +7,14 @@ where
 
 
 import Data.Text (Text, unpack)
+import Data.Text.Read
 import Data.Types
 import Text.XML.Stream.Parse
 import Data.XML.Types
+import Data.Maybe
+import Data.Either
+
+import Data.Monoid ((<>))
 
 import Control.Monad.Trans.Resource
 import Data.Conduit
@@ -33,3 +38,40 @@ importMidiMap path = do
     mm <- runResourceT $
         parseFile def path $$ parseMidiMap
     return mm
+
+
+
+
+--parseInstrument :: MonadThrow m => ConduitM Event o m (Maybe InstrumentFile)
+--parseInstrument =
+    --tagName "instrument" (requireAttr "version" <> requireAttr "name") $ many parseSamples
+
+--parseSamples :: MonadThrow m => ConduitM Event o m (Maybe HitSample)
+--parseSamples =
+    --tagName "sample" (requireAttr "name" <> requireAttr "power") $ many parseAudioFile
+
+
+parseAudioFile :: MonadThrow m => ConduitM Event o m (Maybe AudioFile)
+parseAudioFile = do
+    tagName "audiofile" attrs
+    where
+        attrs = do
+            chan <- requireAttr "channel"
+            file <- requireAttr "file"
+            filechannel <- requireAttr "filechannel"
+            let chan' = (fmap fst . listToMaybe . reads) (unpack chan)
+                filechannel' = decimal filechannel
+
+            if isNothing chan' || isLeft filechannel'
+                then do
+                    return Nothing
+                else do
+                    let Right (x, _) = filechannel'
+                    return $ Just $ AudioFile (fromJust chan') (unpack file) x
+
+
+--importInstrumentFile :: FilePath -> IO (Maybe InstrumentFile)
+--importInstrumentFile path = do
+    --iF <- runResourceT $
+        --parseFile def path $$ parseAudioFile
+    --return iF
